7ad17fcb5ecefbe9aea6108e69d48569a4d7da0d
Bryan Sandoval <bsandovalr1@miumg.edu.gt>
2024-05-25 12:31:20 -0600

    Segundo commit

---
+++ Clasefinal/Data/DataAcces/Consolas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clasefinal.Data.DataAcces
+{
+    internal class Consolas
+    {
+
+    }
+}
